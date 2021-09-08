4329d29ce7f2c34bd92cec20e27edcee7b7c785d
MacBry <maciej.bryja@wp.pl>
2021-09-08 12:53:58 +0200

    add login from mysql

--- src/main/java/com/mac/bry/crud/configurations/SecurityConfiguration.java
+++ src/main/java/com/mac/bry/crud/configurations/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.mac.bry.crud.configurations;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -21,7 +27,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("mac").password("{noop}bry").roles("USER");
+		//in memory user
+		//auth.inMemoryAuthentication().withUser("mac").password("{noop}bry").roles("USER");
+		
+		auth.jdbcAuthentication().dataSource(dataSource).withUser("mac1").password("{noop}bry1").roles("USER");
 	}
 	
 	
--- src/main/java/com/mac/bry/crud/controllers/UserController.java
+++ src/main/java/com/mac/bry/crud/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import com.mac.bry.crud.entities.User;
 import com.mac.bry.crud.repositories.UserRepository;
