8a9bd81acd8fba4faa148b36caac289b026e401d
MacBry <maciej.bryja@wp.pl>
2021-09-14 12:56:08 +0200

    add @Controller to RegisterController.java

--- src/main/java/com/mac/bry/crud/controllers/RegisterController.java
+++ src/main/java/com/mac/bry/crud/controllers/RegisterController.java
@@ -3,6 +3,7 @@ package com.mac.bry.crud.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.mac.bry.crud.entities.User;
 import com.mac.bry.crud.services.UserService;
 
+@Controller
 public class RegisterController {
 	
 	private UserService userService;
