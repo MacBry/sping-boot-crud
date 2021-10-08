a9a4f38f740651a35af93e90ae2f5795ea416fd8
MacBry <maciej.bryja@wp.pl>
2021-10-08 14:33:01 +0200

    Task form

--- src/main/java/com/mac/bry/crud/entities/TaskStatus.java
+++ src/main/java/com/mac/bry/crud/entities/TaskStatus.java
@@ -1,10 +1,10 @@
 package com.mac.bry.crud.entities;
 
 public enum TaskStatus {
-	TASK_COMPLETED("TASK COMPLETED"),
-	TASK_IN_PROGRESS("TASK IN PROGRESS"),
-	TASK_CANCELED("TASK CANCELED"),
-	TASK_POSTPONED("Task POSTPONED");
+	TASK_COMPLETED("Task completed"),
+	TASK_IN_PROGRESS("Task in progress"),
+	TASK_CANCELED("Task canceled"),
+	TASK_POSTPONED("Task postponed");
 	
 	private final String decription;
 	
